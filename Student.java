bc3b52ce5afe78523388a9f404315a52b9c45973
Cory Kennedy <corykennedy@mines.edu>
2019-02-14 12:15:55 -0700

    implemented Comparable added constructor, name and score attribs, setters, and getters

--- Student.java
+++ Student.java
@@ -1,4 +1,41 @@
 
-public class Student {
+public class Student implements Comparable<Student> {
+	private String name = "";
+	private int score = 0;
+	
+	public Student(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
 
+	@Override
+	public int compareTo(Student someStudent) {
+		// if callers score is greater/equal to callees
+		if (this.getScore() >= someStudent.getScore()) {
+			return 1;
+		// if callers score is less than callees
+		} else {
+			return -1;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
+	
 }
