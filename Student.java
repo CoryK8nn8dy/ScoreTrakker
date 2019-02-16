e7841493f2becdf98e8daddace2897197a0cd556
tannercoggins68 <tcoggins@mymail.mines.edu>
2019-02-15 17:20:13 -0700

    Added Student.java

--- Student.java
+++ Student.java
@@ -1,22 +1,22 @@
 
-public class Student implements Comparable<Student> {
-	private String name = "";
+public class Student implements Comparable<Student>{
+	private String name;
 	private int score = 0;
 	
 	public Student(String name, int score) {
-		super();
+//		super();
 		this.name = name;
 		this.score = score;
 	}
 
 	@Override
 	public int compareTo(Student someStudent) {
-		// if callers score is greater/equal to callees
-		if (this.getScore() >= someStudent.getScore()) {
-			return 1;
-		// if callers score is less than callees
-		} else {
-			return -1;
+		
+		if (this.getScore() == someStudent.getScore()) {
+			return 0;
+		}
+		else {
+			return this.score > someStudent.getScore() ? 1 : -1;
 		}
 	}
 
@@ -35,7 +35,18 @@ public class Student implements Comparable<Student> {
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", score=" + score + "]";
+	}
+//
+//	@Override
+//	public int compareTo(Object someStudent) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//	
 	
 	
 }
